740aa9a5a7d033dea9a237a38904804bdfcbe5a4
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-04-11 16:42:42 +0200

    create C and use on page

---
+++ app/components/c.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import A from "./a";
+import B from "./b";
+
+const C = () => {
+  return (
+    <>
+      <A />
+      <B />
+    </>
+  );
+};
+
+export default C;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import C from "./components/c";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <C />
+      </div>
     </main>
   );
 }
